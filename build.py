65d2e4fbda7c6c9f21cb68d0e5324682e78b6ccc
Evgeny Timofeyev <nawasington@ya.ru>
2018-03-27 18:02:55 +0300

    страница заявок и стили

--- build.py
+++ build.py
@@ -2,6 +2,9 @@ from staticjinja import make_site
 
 
 if __name__ == "__main__":
-    site = make_site()
-    # enable automatic reloading
-    site.render()
+    # site = make_site()
+    index = make_site(contexts=[('index.html', {})])
+    requests = make_site(contexts=[('requests.html', {'show_header_logo': True})])
+
+    index.render()
+    requests.render()
