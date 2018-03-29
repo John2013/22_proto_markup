71188dd6ccfe8f9a83194c7acbcb895b07b3683b
Evgeny Timofeyev <nawasington@ya.ru>
2018-03-29 15:37:45 +0300

    requests show more button

--- build.py
+++ build.py
@@ -2,8 +2,8 @@ from staticjinja import make_site
 
 
 if __name__ == "__main__":
-    # site = make_site()
     index = make_site(contexts=[('index.html', {})])
+
     requests = make_site(contexts=[('requests.html', {
         'show_header_logo': True,
         'pages': [
